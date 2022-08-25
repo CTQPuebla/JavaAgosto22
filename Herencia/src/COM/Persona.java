97e19a4406730f8662f3ca6fce3f147774727384
Axel23081195 <hernandezaxel91@outlook.com>
2022-08-25 09:54:52 -0500

    Practica_Herencia

---
+++ Herencia/src/COM/Persona.java
@@ -0,0 +1,75 @@
+package COM;
+
+public  class Persona {
+	
+	//Atributos
+	public String nombre;
+	private int edad;
+	 protected String sexo;
+	String nacionalidad;
+	
+	public Persona() {
+		
+		
+	}
+
+	public Persona(String nombre, int edad, String sexo, String nacionalidad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", edad=" + edad + ", sexo=" + sexo + ", nacionalidad=" + nacionalidad
+				+ "]";
+	}
+	
+	//Metodos propios 
+	public void caminar() {
+		
+	}
+	public void hablar() {
+		
+	}
+	
+	public void pensar() {
+		
+	}
+	
+
+}
