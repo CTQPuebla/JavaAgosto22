db872c1560cc9628bc8a67b4fd198194a238677b
Francisco7587 <christoferfuentesislas@gmsil.com>
2022-08-25 09:48:55 -0500

    Herencia_CFFI

---
+++ Herencia_CFFI/src/com/Empleado.java
@@ -0,0 +1,95 @@
+package com;
+
+public class Empleado extends Persona{
+	
+	int idEmpleado;
+     String nss;
+	double salario;
+	
+	
+	public Empleado() {
+	
+	}
+
+
+	public Empleado(String nombre, int edad, String sexo, String nacionalidad, int idEmpleado, String nss,
+			double salario) {
+		super(nombre, edad, sexo, nacionalidad);
+		this.idEmpleado = idEmpleado;
+		this.nss = nss;
+		this.salario = salario;
+	}
+
+
+	public int getIdEmpleado() {
+		return idEmpleado;
+	}
+
+
+	public void setIdEmpleado(int idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+
+
+	public String getNss() {
+		return nss;
+	}
+
+
+	public void setNss(String nss) {
+		this.nss = nss;
+	}
+
+
+	public double getSalario() {
+		return salario;
+	}
+
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Empleado [idEmpleado=" + idEmpleado + ", nss=" + nss + ", salario=" + salario + "]";
+	}
+
+	public  void trabajar (String Herramienta) {
+
+		System.out.println("Trabajando con: " + Herramienta);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
